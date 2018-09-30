b2b6ba6b772fd9ab815504f9b370021cda8eafe6
Skeep <Skeep747@gmail.com>
2018-09-30 19:20:17 +0300

    Fin

--- LinkClicker/MainWindow.xaml.cs
+++ LinkClicker/MainWindow.xaml.cs
@@ -1,18 +1,40 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace LinkClicker
 {
     public partial class MainWindow
     {
+        private readonly Stack<string> _links;
+
         public MainWindow()
         {
             InitializeComponent();
+            _links = new Stack<string>(File.ReadAllText("links.txt").Replace("\r", "").Split('\n').Reverse());
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            Process.Start("http://google.com");
+            try
+            {
+                Process.Start(_links.Pop());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ссылки закончились! А если нет то проверь правильность ссылки =)");
+            }
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            var str = string.Empty;
+            str = _links.ToArray().Aggregate(str, (current, s) => current + (s + "\r\n"));
+            File.WriteAllText("links.txt", str);
         }
     }
 }
